0585daecce613937252f4afb80b01ec143d6a655
Doan Minh Duy <duydoan812@gmail.com>
2025-04-12 16:59:41 +0700

    12/04/2025_Update_Commit

--- CodeGym_GameProject_01/Assets/Scripts/GroundSpawner.cs
+++ CodeGym_GameProject_01/Assets/Scripts/GroundSpawner.cs
@@ -9,14 +9,18 @@ public class GroundSpawner : MonoBehaviour
 
     public void SpawnTile()
     {
+        
+
         GameObject temp = Instantiate(groundTile, nextSpawnPoint, Quaternion.identity);
         nextSpawnPoint = temp.transform.GetChild(1).transform.position;
+        GroundTile tileScript = temp.GetComponent<GroundTile>();
+        tileScript.SpawnObstacles();
+
     }
 
     // Start is called before the first frame update
     void Start()
     {
-       SpawnTile();
     }
 
     // Update is called once per frame
--- CodeGym_GameProject_01/Assets/Scripts/GroundTile.cs
+++ CodeGym_GameProject_01/Assets/Scripts/GroundTile.cs
@@ -14,12 +14,30 @@ public class GroundTile : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        groundSpawner.SpawnTile();
-        Destroy(gameObject, 3);
+        //if (other.gameObject.CompareTag("Player"))
+        {
+            groundSpawner.SpawnTile();
+        }
+           
     }
     // Update is called once per frame
     void Update()
     {
         
     }
+
+    public Transform horizonobstacles_Spawnpoint;
+    public GameObject horizonobstaclesPrefab;
+    public Transform verticalobstacles_Spawnpoint;
+    public GameObject verticalobstaclesPrefab;
+    public void SpawnObstacles()
+    {
+        //Choose a random point to spawn the obstacles
+        int obstaclesSpawnIndex = Random.Range(2,4);
+        Transform spawnPoint = transform.GetChild(obstaclesSpawnIndex).transform;
+        //Spawn the obstacles at the position
+        Instantiate(horizonobstaclesPrefab, horizonobstacles_Spawnpoint.position, Quaternion.identity, transform);
+        Instantiate(verticalobstaclesPrefab, verticalobstacles_Spawnpoint.position, Quaternion.identity,transform);
+    }
+
 }
